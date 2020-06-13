120ec220218ba40562e786abbf4e179813643e05
NeeT-T <renato.santojpg@gmail.com>
2020-06-13 00:16:55 -0300

    .

--- Code A5/Paradigma A5.pl
+++ Code A5/Paradigma A5.pl
@@ -7,21 +7,47 @@ cor_do_pelo(_).
 corpo(_).
 tamanho(_).
 nacionalidade(_).
+trabalho(_).
 expectativa_maxima_de_vida(_).
 
 familia(_).
 
-cachorro(dogo) :- familia(esportista), aparencia(bravo), caracteristica(_),
-    caracteristicas_incomum(mancha_no_olho), cor_dos_olhos(preto), pelo(curto),
-    cor_do_pelo(branco), corpo(robusto), tamanho(grande), nacionalidade(argentina),
-    expectativa_maxima_de_vida('12'), linhagem_sanguinea(cachorro(dogo)).
+cachorro(dogo) :-
+    familia(esportista),
+    aparencia(amedrontadora),
+    caracteristicas(dogo),
+    caracteristicas_incomum(mancha_no_olho),
+    cor_dos_olhos(preto),
+    pelo(curto),
+    cor_do_pelo(branco),
+    corpo(robusto),
+    tamanho(grande),
+    nacionalidade(argentina),
+    proficiencia(_),
+    expectativa_maxima_de_vida('12'),
+    linhagem_sanguinea(cachorro(dogo)).
 
-cachorro(dogue) :- familia(trabalhadores), caracteristica(territorialista), cor_dos_olhos(preto),
-    , corpo(longo), corpo(robusto),
-    pelo(curto), nacionalidade(alemanha), pelagem(dogue), expectativa_maxima_de_vida('10').
+cachorro(dogue) :-
+    familia(trabalhadores),
+    aparencia(amedrontadora),
+    caracteristicas(dogue),
+    cor_dos_olhos(preto),
+    pelo(curto),
+    pelagem(dogue),
+    corpo(robusto),
+    tamanho(grande),
+    nacionalidade(alemanha),
+    proficiencia(dogue),
+    expectativa_maxima_de_vida('10').
 
-pelagem(dogue) :- cor_do_pelo(X), member(X, [preto, tigrado, castanho, azul]).
+caracteristicas(dogo) :- caracteristica(X), member(X, [afetuoso, fiel, protetor, alegre, amigavel, tolerante]).
+caracteristicas(dogue) :- caracteristica(X), member(X, [dedicado, amigavel, amoroso, reservado, confiante, gentil]).
 
-linhagem_sanguinea(cachorro(dogo)) :- cachorro(dogue), cachorro(boxer),
+proficiencia(dogo) :- trabalho(X), member(X, [guarda, cacador, guia]).
+proficiencia(dogue) :- trabalho(X), member(X, [guarda, cacador]).
+
+pelagem(dogue) :- cor_do_pelo(X), member(X, [preto, tigrado, castanho, azul, manto, arlequim]).
+
+linhagem_sanguinea(cachorro(dogo)) :- cachorro(dogue), cachorro(cachorro_lutador_de_cordoba),
     cachorro(mastin), cachorro(bulldog), cachorro(bull_terrier),
     cachorro(mastim_dos_pirineus).

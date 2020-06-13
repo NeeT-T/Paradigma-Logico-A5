817a4d03bce9f57126f18cd3bec432055c20e9f5
NeeT-T <renato.santojpg@gmail.com>
2020-06-13 16:38:17 -0300

    .

--- Code A5/Paradigma A5.pl
+++ Code A5/Paradigma A5.pl
@@ -1,144 +1,179 @@
 caracteristica(_).
-trabalho(_).
+proficiencia(_).
 expectativa_maxima_de_anos_de_vida(_).
 caracteristicas_corporal_incomum(_).
 cor_dos_olhos(_).
-pelo(_).
+continente(_).
+pelagem(_).
 cor_do_pelo(_).
 corpo(_).
 tamanho(_).
 altura_media_maxima_cm(_).
 peso_medio_maximo_kg(_).
-pais_de_origem(argentina).
+
 
 %---------------------------------------------Machos-----------------------------------------------
 
 cachorro(dogo) :-
-    proficiencia(dogo),
+    proficiencias(dogo),
     caracteristicas(dogo),
     expectativa_maxima_de_anos_de_vida('12'),
-    nacionalidade(argentino),
+    pais_de_origem(argentina),
     caracteristicas_corporal_incomum(mancha_no_olho),
     cor_dos_olhos(preto),
-    pelo(curto),
+    pelagem(curto),
     cor_do_pelo(branco),
     corpo(robusto),
     tamanho(grande),
     altura_media_maxima_cm('68'),
-    peso_medio_maximo_kg('45'),
-    linhagem_sanguinea(dogo).
+    peso_medio_maximo_kg('45').
 
 cachorro(dogue_alemao) :-
-    proficiencia(dogue_alemao),
+    proficiencias(dogue_alemao),
     expectativa_maxima_de_anos_de_vida('12'),
     caracteristicas(dogue_alemao),
     caracteristicas_corporal_incomum(_),
     cor_dos_olhos(preto),
-    pelo(curto),
-    pelagem(dogue_alemao),
+    pelagem(curto),
+    pelo(dogue_alemao),
     corpo(robusto),
     tamanho(grande),
     altura_media_maxima_cm('79'),
     peso_medio_maximo_kg('82'),
-    nacionalidade(alemao).
+    pais_de_origem(alemanha).
 
 cachorro(lutador_de_cordoba) :-
-    proficiencia(lutador_de_cordoba),
+    proficiencias(lutador_de_cordoba),
     expectativa_maxima_de_anos_de_vida(uma_luta),
     caracteristica(agrecivo),
     caracteristicas_corporal_incomum(musculatura_extraordinaria),
     cor_dos_olhos(preto),
-    pelo(curto),
-    pelagem(lutador_de_cordoba),
+    pelagem(curto),
+    pelo(lutador_de_cordoba),
     corpo(robusto),
     tamanho(grande),
     altura_media_maxima_cm('61'),
     peso_medio_maximo_kg('36'),
-    nacionalidade(argentino).
+    pais_de_origem(argentina).
 
 cachorro(boxer) :-
-    proficiencia(boxer),
+    proficiencias(boxer),
     expectativa_maxima_de_anos_de_vida('12'),
     caracteristicas(boxer),
     caracteristicas_corporal_incomum(_),
-    cor_dos_olhos(marron_escuro),
-    pelo(curto),
-    pelagem(boxer),
+    cor_dos_olhos(castanho),
+    pelagem(curto),
+    pelo(boxer),
     corpo(robusto),
     tamanho(grande),
     altura_media_maxima_cm('62'),
     peso_medio_maximo_kg('35'),
-    nacionalidade(alemao).
+    pais_de_origem(alemanha).
 
 cachorro(irish_wolfhound) :-
-    proficiencia(irish_wolfhound),
+    proficiencias(irish_wolfhound),
     expectativa_maxima_de_anos_de_vida('8'),
     caracteristicas(irish_wolfhound),
     caracteristicas_corporal_incomum(_),
     cor_dos_olhos(preto),
-    pelo(curto),
-    pelagem(irish_wolfhound),
+    pelagem(curto),
+    pelo(irish_wolfhound),
     corpo(robusto),
     tamanho(grande),
     altura_media_maxima_cm('203'),
     peso_medio_maximo_kg('82'),
-    nacionalidade(irlandes).
+    pais_de_origem(irlanda).
+
+cachorro(buldogue) :-
+    proficiencia(caçador),
+    expectativa_maxima_de_anos_de_vida('12'),
+    caracteristicas(buldogue),
+    caracteristicas_corporal_incomum(_),
+    cor_dos_olhos(preto),
+    pelagem(curto),
+    pelo(buldogue),
+    corpo(robusto),
+    tamanho(medio),
+    altura_media_maxima_cm('40'),
+    peso_medio_maximo_kg('25'),
+    pais_de_origem(inglaterra).
+
+
+cachorro(bull_terrier) :-
+    proficiencias(bull_terrier),
+    expectativa_maxima_de_anos_de_vida('14'),
+    caracteristicas(bull_terrier),
+    caracteristicas_corporal_incomum(musculosa_e_simetrica),
+    cor_dos_olhos(preto),
+    pelagem(curto),
+    pelo(bull_terrier),
+    corpo(robusto),
+    tamanho(medio),
+    altura_media_maxima_cm('55'),
+    peso_medio_maximo_kg('29'),
+    pais_de_origem(inglaterra).
+
 
 %--------------------------------------------------Femeas-------------------------------------------
 
 %--------------------------------------------------Caracteristicas-----------------------------------
 
 caracteristicas(dogo) :- caracteristica(X),
-    member(X, [afetuoso, fiel, protetor, alegre, amigavel, tolerante]), !.
+    member(X, [afetuoso, protetor, fiel, alegre, amigavel, tolerante]), !.
 caracteristicas(dogue_alemao) :- caracteristica(X),
     member(X, [dedicado, amigavel, amoroso, reservado, confiante, gentil]), !.
 caracteristicas(boxer) :- caracteristica(X),
-    member(X, [afetuoso, desconfiado, fiel, protetor]), !.
+    member(X, [dedicado, destemido, brincalhao, inteligente, fiel, confiante, alegre, brilhante, bravo, energetico, amigavel, calmo]), !.
 caracteristicas(irish_wolfhound) :- caracteristica(X),
-    member(X, [reservado, fiel, protetor]), !.
+    member(X, [pensativo, dignificado, generoso, paciente, fiel, doce]), !.
+caracteristicas(buldogue) :- caracteristica(X),
+    member(X, [afetuoso, protetor, brincalhao, amigavel]), !.
+caracteristicas(bull_terrier) :- caracteristica(X),
+    member(X, [protetor, ativo, afiado, treinavel]), !.
 
 %--------------------------------------------------Trabalho----------------------------------------
 
-proficiencia(dogo) :- trabalho(X),
+proficiencias(dogo) :- proficiencia(X),
     member(X, [guarda, caçador, guia, policial, resgate, schutzhund]), !.
-proficiencia(dogue_alemao) :- trabalho(X),
+proficiencias(dogue_alemao) :- proficiencia(X),
     member(X, [caçador, guia, policial]), !.
-proficiencia(lutador_de_cordoba) :- trabalho(X),
+proficiencias(lutador_de_cordoba) :- proficiencia(X),
     member(X, [guarda, caçador]), !.
-proficiencia(boxer) :- trabalho(X),
-    member(X, [guarda, guia]), !.
-proficiencia(irish_wolfhound) :- trabalho(X),
+proficiencias(boxer) :- proficiencia(X),
+    member(X, [guarda, guia, caçador]), !.
+proficiencias(irish_wolfhound) :- proficiencia(X),
+    member(X, [guarda, caçador]), !.
+proficiencias(bull_terrier) :- proficiencia(X),
     member(X, [guarda, caçador]), !.
 
 %-----------------------------------------------Cor do Pelo-----------------------------------------
 
-pelagem(dogue_alemao) :- cor_do_pelo(X),
+pelo(dogue_alemao) :- cor_do_pelo(X),
     member(X, [preto, tigrado, castanho, azul, manto, arlequim]), !.
-pelagem(lutador_de_cordoba) :- cor_do_pelo(X),
+pelo(lutador_de_cordoba) :- cor_do_pelo(X),
     member(X, [branco, pirata]), !.
-pelagem(boxer) :- cor_do_pelo(X),
-    member(X, [tigrado, dourado]), !.
-pelagem(irish_wolfhound) :- cor_do_pelo(X),
+pelo(boxer) :- cor_do_pelo(X),
+    member(X, [tigrado, dourado, branco]), !.
+pelo(irish_wolfhound) :- cor_do_pelo(X),
     member(X, [cinza, tigrado, preto, branco, castanha_e_trigo]), !.
+pelo(buldogue) :- cor_do_pelo(X),
+    member(X, [caramelo, tigrado, fulvo, branco, malhado]), !.
+pelo(bull_terrier) :- cor_do_pelo(X),
+    member(X, [branco, preto, marron]), !.
 
 %-------------------------------------------------Continente----------------------------------------
 
-continente(europa) :- pais_de_origem(X),
-    member(X, [alemanha, irlanda]), !.
-continente(sul_america) :- pais_de_origem(X),
-    member(X, [argentina]), !.
-
-nacionalidade(alemao):- continente(europa).
-nacionalidade(argentino):- continente(sul_america).
-nacionalidade(irlandes) :- continente(europa).
+pais_de_origem(alemanha):- continente(europeu).
+pais_de_origem(argentina):- continente(sul_americano).
+pais_de_origem(irlanda):- continente(europeu).
+pais_de_origem(inglaterra):- continente(europeu).
 
 %-----------------------------------------------Linhagem sanguinea----------------------------------
 
 % O dogo ainda tem muito mais cachorros em sua linhagem porém ficaria muito grande e não saudavel
 linhagem_sanguinea(dogo) :- 
-    cachorro(dogue_alemao),
-    cachorro(lutador_de_cordoba),
-    cachorro(irish_wolfhound),
-    cachorro(boxer).
-
+    caracteristicas(dogue_alemao),
+    caracteristica(lutador_de_cordoba),
+    caracteristicas(irish_wolfhound),
+    caracteristicas(boxer).
 

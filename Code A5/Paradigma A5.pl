b10f603abae0f6548eea3f55b95b9890f6e11491
NeeT-T <renato.santojpg@gmail.com>
2020-06-14 13:47:32 -0300

    .

--- Code A5/Paradigma A5.pl
+++ Code A5/Paradigma A5.pl
@@ -1,19 +1,19 @@
 caracteristica(_).
-proficiencia(_).
 expectativa_maxima_de_anos_de_vida(_).
-caracteristicas_corporal_incomum(_).
-cor_dos_olhos(_).
+proficiencia(_).
 ancestral(_).
 continente(_).
 pais_de_origem(_).
+caracteristicas_corporal_incomum(_).
 pelagem(_).
+cor_dos_olhos(_).
 cor_do_pelo(_).
 corpo(_).
 tamanho(_).
 altura_media_cm(_).
 peso_medio_kg(_).
 
-%Machos
+%---------------------------------------------Machos-----------------------------------------------
 
 cachorro(dogo) :-
     proficiencias(dogo),
@@ -91,7 +91,7 @@ cachorro(irish_wolfhound) :-
     linhagem_genetica(irish_wolfhound, _).
 
 cachorro(bulldog) :-
-    proficiencia(caçador),
+    proficiencias(bulldog),
     expectativa_maxima_de_anos_de_vida('12'),
     caracteristicas(bulldog),
     caracteristicas_corporal_incomum(_),
@@ -121,7 +121,7 @@ cachorro(bull_terrier) :-
     regiao_origem(birminghan),
     linhagem_genetica(bull_terrier, _).
 
-% Caracteristicas
+%--------------------------------------------------Caracteristicas-----------------------------------
 
 caracteristicas(dogo) :- caracteristica(X),
     member(X, [afetuoso, protetor, fiel, alegre, amigavel, tolerante]), !.
@@ -136,7 +136,7 @@ caracteristicas(bulldog) :- caracteristica(X),
 caracteristicas(bull_terrier) :- caracteristica(X),
     member(X, [protetor, ativo, afiado, treinavel]), !.
 
-% Proficiencias
+%--------------------------------------------------proficiencias----------------------------------------
 
 proficiencias(dogo) :- proficiencia(X),
     member(X, [guarda, caçador, guia, policial, resgate, schutzhund]), !.
@@ -148,10 +148,12 @@ proficiencias(boxer) :- proficiencia(X),
     member(X, [guarda, guia, caçador, policial]), !.
 proficiencias(irish_wolfhound) :- proficiencia(X),
     member(X, [guarda, caçador]), !.
+proficiencias(bulldog) :- proficiencia(X),
+    member(X, [caçador]), !.
 proficiencias(bull_terrier) :- proficiencia(X),
     member(X, [guarda, caçador]), !.
 
-% Cor do Pelo
+%-----------------------------------------------Cor do Pelo-----------------------------------------
 
 pelo(dogue_alemao) :- cor_do_pelo(X),
     member(X, [preto, tigrado, castanho, azul, manto, arlequim]), !.
@@ -166,7 +168,7 @@ pelo(bulldog) :- cor_do_pelo(X),
 pelo(bull_terrier) :- cor_do_pelo(X),
     member(X, [branco, preto, marron]), !.
 
-% Origem
+%-----------------------------------------------Continente----------------------------------------
 
 regiao_origem(cordoba):- continente(sul_americano), pais_de_origem(argentina).
 regiao_origem(hamburgo):- continente(europeu), pais_de_origem(alemanha).
@@ -175,18 +177,22 @@ regiao_origem(dublin):- continente(europeu), pais_de_origem(irlanda).
 regiao_origem(nottingham):- continente(europeu), pais_de_origem(inglaterra).
 regiao_origem(birminghan):- continente(europeu), pais_de_origem(inglaterra).
 
-% Linhagem sanguinea
+%-----------------------------------------------Linhagem sanguinea----------------------------------
+
+% O dogo ainda tem muito mais cachorros em sua linhagem porém ficaria muito grande e não saudavel
 linhagem_genetica(dogo, X):- ancestral(X),
     member(X, [lutador_de_cordoba, dogue_alemao, boxer, mastim_espanhol, bulldog, bull_terrier, irish_wolfhound, mastim_dos_pirineus, dogue_de_bordeaux]), !.
 linhagem_genetica(dogue_alemao, X):- ancestral(X),
     member(X, [mastim_ingles, galgo]), !.
 linhagem_genetica(lutador_de_cordoba, X):- ancestral(X),
-    member(X, [mastin, bull_terrier, boxer, e bulldog]), !.
+    member(X, [mastin, bull_terrier, boxer, bulldog]), !.
 linhagem_genetica(boxer, X):- ancestral(X),
-    member(X, []), !.
+    member(X, [buldogue_alemao]), !.
 linhagem_genetica(irish_wolfhound, X):- ancestral(X),
-    member(X, []), !.
+    member(X, [sloughis, galgo_escoces]), !.
 linhagem_genetica(bulldog, X):- ancestral(X),
-    member(X, []), !.
+    member(X, [alao]), !.
 linhagem_genetica(bull_terrier, X):- ancestral(X),
-    member(X, []), !.
+    member(X, [bulldog, english_white_terrier]), !.
+
+
